dac29a192fc117edc804a540917df5ab7b4ef3fa
MadhaviGunapati610 <59458995+MadhaviGunapati610@users.noreply.github.com>
2020-02-14 18:16:07 -0500

    normal input update using state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
 import "./styles.css";
-
-export default class App extends React.Component{
+ 
+class App extends React.Component{
 constructor(props){
 super(props);
 
+this.state={
+  value:'',
+  output:''
 }
-  render() {
+}
+
+handleInput=(e)=>{
+e.preventDefault();
+this.setState({value:e.target.value});
+}
+handleSubmit=(e)=>{
+  e.preventDefault();
+this.setState({output:this.state.value});
+}
+
+
+
+  render () {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+<p>Output: {this.state.output}</p>
+      <form onSubmit={this.handleSubmit}>
+<input type="text" value={this.value} onChange={this.handleInput}/>
+<input type="submit" value="submit"/>
+      </form>
     </div>
   );
 }
 }
+export default App;
